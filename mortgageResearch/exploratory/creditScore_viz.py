9181b8e89f3b5661dbe401489a3e6551d2c8d702
Peter Altamura <peteraltamura@Altamura-Air.frontier.com>
2017-05-20 23:39:45 -0400

    creditScore_viz -- widened figure before saving to see full distrs for lenders

--- mortgageResearch/exploratory/creditScore_viz.py
+++ mortgageResearch/exploratory/creditScore_viz.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import glob
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,13 +14,19 @@ from reference import (
     originationFileColList, monthlyFileColList, compress_columns
 )
 
-# Define out Variables
-figure_dir = '/Users/peteraltamura/Documents/GitHub/mortgageResearch/Figs/'
-sample_dir = "/Users/peteraltamura/Documents/GitHub/mortgageResearch/Data/" \
-             "historical_data1_Q12016/"
-sample_file = "historicalData1_Q12016.csv"
-sample_file_monthly = "historicalDataMonthly1_Q12016.csv"
+# Sample or Full
+sample_run = False
 
+# Single Dataset Dirs
+figure_dir = '/Users/peteraltamura/Documents/GitHub/mortgageResearch/Figs/'
+sample_single_dir = "/Users/peteraltamura/Documents/GitHub/" \
+                    "mortgageResearch/Data/single/"
+sample_single_file = "historical_data1_Q12016.txt"
+
+# Monthly Dataset Dirs
+sample_monthly_dir = '/Users/peteraltamura/Documents/GitHub/' \
+                     'mortgageResearch/Data/monthly/"'
+sample_file_monthly = "historicalDataMonthly1_Q12016.txt"
 
 
 if __name__ == "__main__":
@@ -27,17 +34,41 @@ if __name__ == "__main__":
     if not os.path.exists(figure_dir):
         os.makedirs(figure_dir)
 
-    df_origination = load_data(
-        path=sample_dir,
-        filename=sample_file,
-        columns=originationFileColList,
-        date_col_fmt_dict={'firstPaymentDate': '%Y%m'}
-    )
-    print df_origination.columns
+    # Determine files to read in
+    if sample_run:
+
+        # Needs parser for pandas.io.common.CParserError 16364
+        df_origination = load_data(
+            path=sample_single_dir,
+            filename=sample_single_file,
+            columns=originationFileColList,
+            nrows=None,
+            date_col_fmt_dict={'firstPaymentDate': '%Y%m'},
+            error_bad_lines=True,
+            engine='c'
+        )
+
+    # Append all otherwise
+    elif not sample_run:
+        df_origination = pd.DataFrame()
+        for path in glob.glob(sample_single_dir + '*.txt'):
+            df_origination = pd.concat(
+                [df_origination,
+                load_data(
+                    path=sample_single_dir,
+                    filename=sample_single_file,
+                    columns=originationFileColList,
+                    nrows=None,
+                    date_col_fmt_dict={'firstPaymentDate': '%Y%m'},
+                    error_bad_lines=True
+                )],
+                axis=0
+            )
 
     # Overall Credit Score Distribution
-    plt.figure(1, figsize=(12, 14))
-    plt.hist(pd.Series(df_origination['creditScore']))
+    plt.figure(1, figsize=(100, 14))
+    plt.hist(pd.Series(df_origination['creditScore']),
+             alpha=0.7)
     plt.title('Credit Score Distribution')
     plt.xlabel('Credit Score')
     plt.ylabel('Frequency')
@@ -46,12 +77,15 @@ if __name__ == "__main__":
     fig_all.savefig(figure_dir + 'All Credit Score Dist.png')
 
     # Credit Score Distribution by Lender
-    plt.figure(2, figsize=(12, 14))
+    plt.figure(2, figsize=(100, 14))
     df_lender = df_origination[['servicerName', 'creditScore']]
     for lender in list(set(df_lender['servicerName'])):
-        plt.hist(pd.Series(list(df_lender.loc[df_lender['servicerName'] == lender, :]['creditScore'])),
-                 label=str(lender),
-                 alpha=0.7)
+        plt.hist(
+            pd.Series(list(df_lender.loc[df_lender['servicerName'] == lender, :]
+                           ['creditScore'])),
+            label=str(lender),
+            alpha=0.7
+        )
     plt.legend(prop={'size': 16})
     plt.title('Credit Score Distribution by Lender')
     plt.xlabel('Credit Score')
@@ -59,5 +93,8 @@ if __name__ == "__main__":
     fig_lender = plt.gcf()
     fig_lender.show()
     fig_lender.savefig(figure_dir + 'Credit Score Dist by Lender.png')
+
+    # Create Lender Trend of Avg Credit Score over time
+
 
 
