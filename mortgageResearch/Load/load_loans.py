ea27c66495523cfd214a1823277df3abd12a1ccc
Peter Altamura <peteraltamura@Altamura-Air.frontier.com>
2017-05-20 23:38:55 -0400

    load_loans -- added process for full run and not sample, glob for finding files in path dir

--- mortgageResearch/Load/load_loans.py
+++ mortgageResearch/Load/load_loans.py
@@ -3,25 +3,45 @@ import numpy as np
 import os
 import sys
 
+sys.path.append(
+    "/Users/peteraltamura/Documents/GitHub/"
+    "mortgageResearch/mortgageResearch/Load"
+)
+from clean_table import clean_observations
+
 
 # Pandas IO / Col List from FAQ User Guide
-def load_data(path, filename, nrows='sample', columns=None,
-               date_col_fmt_dict=None, col_types=None):
+def load_data(path, filename, nrows=None, columns=None,
+               date_col_fmt_dict=None, col_types=None, error_bad_lines=False, engine='c'):
     """
 
     """
 
     # ---- ---- ----      ---- ---- ----
     # Read in from File
-    filename += '.csv' if filename[-4:] != '.csv' else ""
-    df_sample = pd.read_csv(
-        path + filename,
-        nrows=10000 if nrows == 'sample' else nrows
-    )
+    filename += '.txt' if filename[-4:] != '.txt' else ""
+    if engine == 'c':
+        df_sample = pd.read_csv(
+            path + filename,
+            nrows=nrows,
+            error_bad_lines=error_bad_lines,
+            delimiter='|'
+        )
+    elif engine == 'python':
+        df_sample = pd.read_csv(
+            path + filename,
+            nrows=nrows,
+            error_bad_lines=error_bad_lines,
+            delimiter='|',
+            engine=engine
+        )
 
     # Assign columns if not in file already
     if columns:
         df_sample.columns = columns
+
+    # Clean all observations found
+    df_sample = clean_observations(source='fannie', table=df_sample)
 
     # Assign Dtypes
     if col_types:
@@ -33,6 +53,7 @@ def load_data(path, filename, nrows='sample', columns=None,
 
         # Iterate over the dict
         for k, v in date_col_fmt_dict.iteritems():
+            df_sample = df_sample.loc[(df_sample[k].apply(lambda x: len(str(x))).isin([5, 6])), :]
             df_sample.loc[:, k] = pd.to_datetime(df_sample[k], format=v)
 
     # Return
