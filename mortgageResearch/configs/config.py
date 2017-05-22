c2d4c8c64a7e03dde58aff67ad19cac1e706b3a4
PeterA182 <Altamura.Peter@gmail.com>
2017-05-21 21:36:22 -0400

    configs -- initial commit -paths input and output for sources

---
+++ mortgageResearch/configs/config.py
@@ -0,0 +1,17 @@
+configs = {
+    'freddie': {
+        'figure_dir': '/Users/peteraltamura/Documents/GitHub/mortgageResearch/Figs/',
+        'sample_single_dir': "/Users/peteraltamura/Documents/GitHub/"
+                    "mortgageResearch/Data/single/",
+        'sample_single_file': "historical_data1_Q12016.txt",
+        'sample_monthly_dir': '/Users/peteraltamura/Documents/GitHub/'
+                     'mortgageResearch/Data/monthly/',
+        'sample_file_monthly': "historicalDataMonthly1_Q12016.txt"
+    },
+    'acs': {
+        'export_dir': '/Users/peteraltamura/Documents/GitHub/mortgageResearch/Data/',
+        'export_file': 'acs_export.csv'
+    }
+    
+}
+
