a9e9409d3d75497378ad5658d2eed6d67f835b27
PeterA182 <Altamura.Peter@gmail.com>
2017-05-22 21:15:43 -0400

    load_loans -- fixed read_csv params in defaults

--- mortgageResearch/Load/load_loans.py
+++ mortgageResearch/Load/load_loans.py
@@ -11,25 +11,25 @@ from clean_table import clean_observations
 
 
 # Pandas IO / Col List from FAQ User Guide
-def load_data(path, filename, nrows=None, columns=None,
+def load_data(path, nrows=None, columns=None,
                date_col_fmt_dict=None, col_types=None, error_bad_lines=False, engine='c'):
     """
 
     """
+    qtr_convert = lambda x: pd.Period(x, freq='Q')
 
     # ---- ---- ----      ---- ---- ----
     # Read in from File
-    filename += '.txt' if filename[-4:] != '.txt' else ""
     if engine == 'c':
-        df_sample = pd.read_csv(
-            path + filename,
+        df_data = pd.read_csv(
+            path,
             nrows=nrows,
             error_bad_lines=error_bad_lines,
             delimiter='|'
         )
     elif engine == 'python':
-        df_sample = pd.read_csv(
-            path + filename,
+        df_data = pd.read_csv(
+            path,
             nrows=nrows,
             error_bad_lines=error_bad_lines,
             delimiter='|',
@@ -38,26 +38,36 @@ def load_data(path, filename, nrows=None, columns=None,
 
     # Assign columns if not in file already
     if columns:
-        df_sample.columns = columns
+        df_data.columns = columns
+
+    # Create Quarter Column from filename
+    df_data.loc[:, 'Quarter'] = path.rstrip(".txt")[-4:] + \
+                                  path.rstrip(".txt")[-6:-4]
+    df_data.loc[:, 'Quarter'] = df_data['Quarter'].apply(qtr_convert)
 
     # Clean all observations found
-    df_sample = clean_observations(source='fannie', table=df_sample)
+    df_data = clean_observations(source='fannie', table=df_data)
+
+    # Numeric creditScore
+    if 'creditScore' in df_data.columns:
+        df_data.loc[:, 'creditScore'] = \
+            pd.to_numeric(df_data['creditScore'])
 
     # Assign Dtypes
     if col_types:
         for k, v in col_types.iteritems():
-            df_sample.loc[:, k] = df_sample[k].astype(v)
+            df_data.loc[:, k] = df_data[k].astype(v)
 
     # Parse Dates if appropriate
     if date_col_fmt_dict:
 
         # Iterate over the dict
         for k, v in date_col_fmt_dict.iteritems():
-            df_sample = df_sample.loc[(df_sample[k].apply(lambda x: len(str(x))).isin([5, 6])), :]
-            df_sample.loc[:, k] = pd.to_datetime(df_sample[k], format=v)
+            df_data = df_data.loc[(df_data[k].apply(lambda x: len(str(x))).isin([5, 6])), :]
+            df_data.loc[:, k] = pd.to_datetime(df_data[k], format=v)
 
     # Return
-    return df_sample
+    return df_data
 
 
 if __name__ == "__main__":
