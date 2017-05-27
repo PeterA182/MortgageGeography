6736f22db3f4df935234c128a4ecdd77bb044c93
PeterA182 <Altamura.Peter@gmail.com>
2017-05-27 19:42:05 -0400

    config -- maxBinOccupancy and other directory added for timeSeries

--- mortgageResearch/configs/config.py
+++ mortgageResearch/configs/config.py
@@ -6,7 +6,10 @@ configs = {
         'sample_single_file': "historical_data1_Q12016.txt",
         'sample_monthly_dir': '/Users/peteraltamura/Documents/GitHub/'
                      'mortgageResearch/Data/monthly/',
-        'sample_file_monthly': "historicalDataMonthly1_Q12016.txt"
+        'sample_file_monthly': "historicalDataMonthly1_Q12016.txt",
+        'binned_monthly_dir': "/Users/peteraltamura/Documents/GitHub/"
+                              "mortgageResearch/Data/monthly/bins/",
+        'maxBinOccupancy': 10000
     },
     'acs': {
         'export_dir': '/Users/peteraltamura/Documents/GitHub/mortgageResearch/Data/',
