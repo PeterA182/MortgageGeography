fb3f1ae609370013e2edd48a2e7249fefad33bcc
PeterA182 <Altamura.Peter@gmail.com>
2017-12-30 11:10:00 -0500

    Renamed files to reorg for future combining of Load/FE/Model/ResidualAnalysis into run.py scripts

--- mortgageResearch/model/orig_xgb.py
+++ mortgageResearch/model/orig_xgb.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.path.append(
     "/Users/peteraltamura/Documents/GitHub/"
     "mortgageResearch/mortgageResearch/configs/"
@@ -16,13 +15,20 @@ sys.path.append(
     "/Users/peteraltamura/Documents/GitHub/"
     "mortgageResearch/mortgageResearch/featureEngineering/"
 )
-from b_combinedFeatures import combined_filename, idx_cols, delinq_days_min
+from b_combinedFeatures import combined_filename, idx_cols, \
+    origFE_filename, delinq_days_min
 
 import os
 import pandas as pd
+from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot
+
+
+"""
+Using observations from loanOrigination to determine future default
+"""
+
 
 # Variables
 test_size = .33
@@ -35,7 +41,7 @@ if not os.path.exists(model_outpath):
 # Methods
 def get_results_table(X_test, Y_test, Y_hat):
     """
-
+    
     :param X_test: 
     :param Y_test: 
     :return: 
@@ -65,18 +71,34 @@ def get_results_table(X_test, Y_test, Y_hat):
     return df_result
 
 
-def logisticRegression_model(X_train, Y_train, X_test, Y_test, idx_cols=idx_cols):
+def xgb_model(X_train, Y_train, X_test, Y_test, idx_cols=idx_cols):
     """
-
+    
     :param X: 
     :param Y: 
     :return: 
     """
 
-    # Instantiate and Fit Model
-    model = LogisticRegression()
-    model.fit(X_train.loc[:, [x for x in X_train.columns if x not in idx_cols]],
-              Y_train)
+    X_train_sub = X_train.loc[:, [
+        x for x in X_train.columns if x not in idx_cols
+    ]]
+    for c in X_train_sub.columns:
+        print c
+
+        if sum(X_train_sub[c].isnull()) != 0:
+            print "     {} null observations".format(
+                str(sum(X_train_sub[c].isnull()))
+            )
+
+    for c in Y_train.columns:
+        print c
+        assert sum(Y[c].isnull()) == 0
+
+    model = XGBClassifier()
+    model.fit(
+        X_train_sub,
+        Y_train
+    )
     print model
 
     # Y-hat Series
@@ -105,7 +127,7 @@ if __name__ == "__main__":
     ]]
     X = df_comb.loc[:,
         [x for x in df_comb.columns if x not in Y.columns]
-        ]
+    ]
     for c in X.columns:
         if sum(X[c].isnull()) > 0:
             print c
@@ -126,15 +148,15 @@ if __name__ == "__main__":
             print c
 
     # Train and Test Model
-    print ":: Instantiating and training model ::"
-    model, df_result = logisticRegression_model(X_train=X_train,
-                                                Y_train=Y_train,
-                                                X_test=X_test,
-                                                Y_test=Y_test)
+    print "::Instantiating and training model ::"
+    model, df_result = xgb_model(X_train=X_train,
+                                 Y_train=Y_train,
+                                 X_test=X_test,
+                                 Y_test=Y_test)
 
-    # # Extract Feature Importances DataFrame
-    feat_imp = pd.DataFrame(model.coef_)
-    feat_imp.columns = ['Coefficient']
+    # Extract Feature Importances DataFrame
+    feat_imp = pd.DataFrame(model.feature_importances_)
+    feat_imp.columns = ['importance_value']
     feat_imp['Feature'] = X_test.columns
 
     # Plot Feature Importances
@@ -142,5 +164,8 @@ if __name__ == "__main__":
                model.feature_importances_)
 
     # Send out
-    pyplot.savefig(model_outpath + 'featureImportanceViz.png')
-    df_result.to_csv(model_outpath + 'df_residuals.csv')
+    pyplot.savefig(model_outpath + 'xgb_featureImportanceViz.png')
+    df_result.to_csv(model_outpath + 'xgb_df_residuals.csv')
+    feat_imp.to_csv(model_outpath + 'xgb_featureImportanceTbl.csv')
+
+    print df_result.describe()
